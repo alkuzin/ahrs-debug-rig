2ea5f4aca62f4edf50ecc51d81fd37fc2b52e1fe
Alexander <alkuzindev@gmail.com>
2026-01-07 21:41:54 +0300

    [feat]: added calculate_checksum (RFC 1071) & halt_cpu functions

--- src/stm32-firmware/src/utils.rs
+++ src/stm32-firmware/src/utils.rs
@@ -124,3 +124,38 @@ pub fn generate_payload(state: u32) -> Payload {
 
     payload
 }
+
+/// Calculate Internet checksum (RFC 1071).
+///
+/// # Parameters
+/// - `buffer` - given buffer to handle.
+///
+/// # Returns
+/// - Internet checksum of a given buffer.
+pub fn calculate_checksum(buffer: &[u8]) -> u16 {
+    let mut sum: u32 = 0;
+    let len = buffer.len();
+    let mut i = 0;
+
+    while i < len - 1 {
+        let word = u16::from_ne_bytes([buffer[i], buffer[i + 1]]);
+        sum += word as u32;
+        i += 2;
+    }
+
+    if i < len {
+        sum += buffer[i] as u32;
+    }
+
+    while (sum >> 16) != 0 {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+
+    !(sum as u16)
+}
+
+/// Halt the CPU after a fatal error.
+pub fn halt_cpu() {
+    cortex_m::interrupt::disable();
+    loop { cortex_m::asm::wfi(); }
+}
