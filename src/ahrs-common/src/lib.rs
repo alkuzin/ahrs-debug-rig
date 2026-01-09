245bb646fbe417ae8dbc102cd0d6b947de4c4387
Alexander <alkuzindev@gmail.com>
2026-01-09 14:06:00 +0300

    [info]: added ahrs-common crate for shared declarations among the project modules

---
+++ src/ahrs-common/src/lib.rs
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: Apache-2.0.
+// Copyright (C) 2026-present ahrs-debug-rig project and contributors.
+
+//! Shared declarations among IMU handler & gateway.
+
+#![no_std]
+#![no_main]
+
+pub use idtp;
